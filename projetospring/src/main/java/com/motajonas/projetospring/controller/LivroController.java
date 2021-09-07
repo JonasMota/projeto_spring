864ecc5e82bbb7f7a012f979d50b23a2054f3dc2
JonasMota <jonasmotafeitosa@hotmail.com>
2021-09-07 12:50:49 -0300

    Separação regras de negócio, criação classe Service

--- projetospring/src/main/java/com/motajonas/projetospring/controller/LivroController.java
+++ projetospring/src/main/java/com/motajonas/projetospring/controller/LivroController.java
@@ -3,6 +3,7 @@ package com.motajonas.projetospring.controller;
 import com.motajonas.projetospring.dto.MessageResponseDTO;
 import com.motajonas.projetospring.entity.Livros;
 import com.motajonas.projetospring.repository.LivroRepository;
+import com.motajonas.projetospring.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,18 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/livros")
 public class LivroController {
 
-    private LivroRepository livroRepository;
+    private LivroService livroService;
 
     @Autowired
-    public LivroController(LivroRepository livroRepository) {
-        this.livroRepository = livroRepository;
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
     }
 
     @PostMapping
     public MessageResponseDTO create (@RequestBody Livros livros){
-      Livros savedlivro =  livroRepository.save(livros);
-      return MessageResponseDTO.builder().message("Livro criado com ID" + savedlivro.getId())
-              .build();
+        return livroService.create(livros);
+
 
     }
 }
--- projetospring/src/main/java/com/motajonas/projetospring/service/LivroService.java
+++ projetospring/src/main/java/com/motajonas/projetospring/service/LivroService.java
@@ -1,7 +1,28 @@
 package com.motajonas.projetospring.service;
 
+import com.motajonas.projetospring.dto.MessageResponseDTO;
+import com.motajonas.projetospring.entity.Livros;
+import com.motajonas.projetospring.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class LivroService {
+
+
+    private LivroRepository livroRepository;
+
+    @Autowired
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public MessageResponseDTO create (Livros livros){
+        Livros savedlivro =  livroRepository.save(livros);
+        return MessageResponseDTO.builder().message("Livro criado com ID" + savedlivro.getId())
+                .build();
+
+
+    }
 }
